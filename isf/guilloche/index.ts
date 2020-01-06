b5809d8bb196930e70437f94ea34b7ff4efd8023
spleennooname <spleen666@gmail.com>
2020-01-06 18:29:05 +0100

    1 file modified

--- isf/guilloche/index.ts
+++ isf/guilloche/index.ts
@@ -95,7 +95,7 @@ const float frequency = 30.0;
 const float height = 0.003;
 const float width = 0.02;
 const float alias = 0.002;
-const float bright = 1.8;
+const float bright = 0.75;
 const float dist = 0.2;
 
 mat2 rotate2d(float angle) {
@@ -111,69 +111,23 @@ const float uWeight = 0.6;
 const float uDecay = 0.75;
 vec4 light(vec2 uv, vec2 pos, float t) {
  
-    vec2 tc = uv.xy;
+  vec2 tc = uv.xy;
     
-    vec2 deltaUv = tc - pos.xy;
-    deltaUv *= (1.0 / NUM_SAMPLES * uDensity);
+  vec2 deltaUv = tc - pos.xy;
+  deltaUv *= (1.0 / NUM_SAMPLES * uDensity);
     
-    float illuminationDecay = 1.0;
+  float illuminationDecay = 1.0;
 
-    vec4 color = IMG_NORM_PIXEL(bufferA, tc.xy);
-	
-    tc += deltaUv * fract( sin( dot(uv.xy + fract(t), vec2(12.9898, 78.233)))* 43758.5453 );
-    for (float i = 0.0; i < NUM_SAMPLES; i+=1.0)
-	{
-        tc -= deltaUv;
-        vec4 sampleTex = IMG_NORM_PIXEL(bufferA, tc.xy);
-
-        sampleTex *= illuminationDecay * uWeight;
-        color += sampleTex;
-        illuminationDecay *= uDecay;
-    }
-    
-    return color * vec4(0.5);
-}
-
-vec4 rblur( vec2 uv, vec2 center, float falloffExp){
-    // Translate our floating point space to the center of our blur.
-    uv -= center;
-    // Go ahead and precompute the inverse of the number of samples.
-    // so we don't have any inner divisions.
-    float invSamples = 1.0 / NUM_SAMPLES;
-    
-    // Place to accumulate the result.
-    float result = 0.0;
-    
-    // Independent locations to store the results of each inner tap.
-    // Why? So each tap doesn't need to write back before the next one
-    // can start executing, preventing stalls. Works on x86 and piped
-    // MIPS and I think it helps this out too (at least on my old thinkpad).
-    float r0=0.0,r1=0.0,r2=0.0,r3=0.0;
-    
-    // We need to do each tap at a different index/position, so by storing
-    // them in a vector we can make incrementation a single op instead of 4.
-    vec4 indices = vec4(0,1,2,3);
-    
-	// Same thing with the scale.
-    vec4 scale = vec4(0);
-    
-    // Go through and and sample the texture.
-    for( float i = 0.0; i < NUM_SAMPLES; i+=4.0 ){
-        scale = indices*invSamples;
-        
-        r0 = IMG_NORM_PIXEL(bufferA, (uv*scale.x + center)).g;
-        r1 = IMG_NORM_PIXEL(bufferA, (uv*scale.y + center)).g;
-        r2 = IMG_NORM_PIXEL(bufferA, (uv*scale.z + center)).g;
-        r3 = IMG_NORM_PIXEL(bufferA, (uv*scale.w + center)).g;
-        
-        indices += 4.0;
-        
-        result += r0+r1+r2+r3;
-    }
-    
-    float b = pow(result * invSamples, falloffExp);
-    
-    return vec4(b,b,b,1.);
+  vec4 color = IMG_NORM_PIXEL(bufferA, tc.xy);
+  tc += deltaUv * fract( sin( dot(uv.xy + fract(t), vec2(12.9898, 78.233)))* 43758.5453 );
+  for (float i = 0.0; i < NUM_SAMPLES; i+=1.0){
+    tc -= deltaUv;
+    vec4 sampleTex = IMG_NORM_PIXEL(bufferA, tc.xy);
+    sampleTex *= illuminationDecay * uWeight;
+    color += sampleTex;
+    illuminationDecay *= uDecay;
+  }
+  return color * vec4(0.5);
 }
 
 void main() {
@@ -199,7 +153,7 @@ void main() {
       result += waves;
     }
     result /= levels;
-    result = smoothstep(1.0, 0.0, result);
+    result = smoothstep(0.0, 1.0, result);
     gl_FragColor= vec4(vec3(result), 1.0);
   }
   else if (PASSINDEX == 1){
