ea9caadfaabf9118b5550871bca4d96f7d9ea7e1
spleennooname <spleen666@gmail.com>
2020-03-18 12:38:17 +0100

    2 files modified

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
-console.clear();
-import "./style.scss";
-import { gsap } from "gsap";
 
+import "./style.scss";
+
+import { gsap } from "gsap";
 import { Renderer } from "interactive-shader-format";
 import { isfFragment, isfVertex } from "./isf/guilloche-ray";
 
+// webcam constrains object
 const constraints = {
   audio: false,
   video: {
@@ -12,30 +13,31 @@ const constraints = {
     height: { min: 480 }
   }
 };
+
 // webcam
 let then = window.performance.now();
 let now = 0;
 let delta = 0;
 let time = 0;
 let fps = 60;
+
 let aspectRatio = 1.333;
 const fpsMs = fps / 1000;
+
 const pixelRatio = window.devicePixelRatio || 1;
 
+// dom objects of relevance
 const video: HTMLVideoElement = document.querySelector("#video");
 const canvas: HTMLCanvasElement = document.querySelector("#canvas");
 const cover: HTMLElement = document.querySelector("#cover");
-
-canvas.width = canvas.clientWidth;
-canvas.height = canvas.clientHeight;
-
+// init webgl context
 const gl = canvas.getContext("webgl", {
   powerPreference: "default",
   preserveDrawingBuffer: true,
   desynchronized: true,
   antialias: true
 });
-
+// init ISF renderer
 const renderer = new Renderer(gl);
 try {
   renderer.loadSource(isfFragment, isfVertex);
@@ -44,16 +46,19 @@ try {
 }
 
 const resize = () => {
+  // https://webglfundamentals.org/webgl/lessons/webgl-anti-patterns.html as source of truth
+  // w/h according pixel ratio and canvas size
   const height = Math.round(gl.canvas.clientHeight * pixelRatio);
   const width = Math.round(gl.canvas.clientWidth * pixelRatio);
   const h = Math.round(window.innerHeight);
   const w = Math.round(window.innerWidth);
+  // resize only if necessary
   const needsResize = gl.canvas.width !== width || gl.canvas.height !== height;
   if (needsResize) {
-    if (w < h) {
+    if (w < h) { // porrait
       gl.canvas.width = Math.round(height / aspectRatio);
       gl.canvas.height = height;
-    } else {
+    } else { // landscape
       gl.canvas.width = width;
       gl.canvas.height = Math.round(width / aspectRatio);
     }
@@ -63,6 +68,7 @@ const resize = () => {
 
 const animate = () => {
   window.requestAnimationFrame(animate);
+  // if the video's not ready, skip it
   if (video && video.readyState !== 4) return;
   now = window.performance.now();
   delta = now - then;
@@ -87,7 +93,9 @@ const render = (renderObject = {}) => {
 async function init() {
   try {
     const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    // init video stuff
     success(stream);
+    // start effect here
     then = window.performance.now();
     animate();
   } catch (e) {
@@ -100,22 +108,21 @@ const error = e => {
 };
 
 const success = stream => {
+  // check iOS device
+  const ios = /iPad|iPhone|iPod/.test(navigator.userAgent);
+  // set video 
   video.setAttribute("autoplay", "");
   video.setAttribute("muted", "true");
   video.setAttribute("playsinline", "");
   const videoTracks = stream.getVideoTracks();
   const videoSettings = videoTracks[0].getSettings();
-  const ios = /iPad|iPhone|iPod/.test(navigator.userAgent);
   const videoTracks = stream.getVideoTracks();
   const videoSettings = videoTracks[0].getSettings();
-  // 1st canvas resize, according video size
-  gl.canvas.width = videoSettings.width;
-  gl.canvas.height = videoSettings.height;
-  // compute aspectRatio
+  // ccompute aspect ratio (ios swap height/width)
   aspectRatio = ios
     ? videoSettings.height / videoSettings.width
     : videoSettings.width / videoSettings.height;
-
+  // bind video stream
   window.stream = stream;
   if (video.mozSrcObject !== undefined) {
     video.mozSrcObject = stream;
@@ -128,6 +135,7 @@ const success = stream => {
       video.src = window.URL && window.URL.createObjectURL(stream);
     }
   }
+  // 1st canvas resize, according video settings and aspect ratio
   resize()
 };
 
@@ -138,7 +146,3 @@ cover.addEventListener("click", e => {
     onComplete: () => init()
   });
 });
-
-// rx
-/* const source = of("World").pipe(map(x => `Hello ${x}!`));
-source.subscribe(x => console.log(x)); */
